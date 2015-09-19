97a3efde47ff6a021e61982367be4b2481b4ef58
Rik Dunphy <rik.dunphy@gmail.com>
2015-09-19 15:09:29 -0700

    minor updates to simplify the new script

--- hdhr_md.py
+++ hdhr_md.py
@@ -38,8 +38,24 @@ class HDHomeRunMD:
             
     def get_episode_string(self, showname,epNumber,epAirdate,epTitle):
         episode_str = '00'
+        if not epNumber:
+            # going to have to search for it
+            tvdb = tvdb_api.Tvdb()
+            show = tvdb[showname]
+            for series in show:
+                seriesdata = tvdb[showname][series]
+                for ep in seriesdata:
+                    epData = seriesdata[ep]
+                    ep_date = datetime.datetime.strptime(epData['firstaired'],'%Y-%m-%d')
+                    check_date = datetime.datetime.utcfromtimestamp(int(epAirdate))
+                    if ep_date == check_date:
+                        logging.debug('MATCHED Season [' + str(series) + '] Episode [' + str(ep) + ']')
+                        episode_str = str(ep)
+                        return episode_str
+            return
+                    
         if epNumber[3] == 'E':
-            episode_str = epNumber[3:6]
+            episode_str = epNumber[4:6]
         else:
             # going to have to search for it
             tvdb = tvdb_api.Tvdb()
@@ -52,12 +68,27 @@ class HDHomeRunMD:
                     check_date = datetime.datetime.utcfromtimestamp(int(epAirdate))
                     if ep_date == check_date:
                         logging.debug('MATCHED Season [' + str(series) + '] Episode [' + str(ep) + ']')
-                        episode_str = 'E' + str(ep)
+                        episode_str = str(ep)
                         return episode_str
         return episode_str
         
     def get_season_string(self, showname,epNumber,epAirdate,epTitle):
         season_str = '00'
+        if not epNumber:
+            logging.info('No Episode Details - will have to use the show and airdate to extract')
+            tvdb = tvdb_api.Tvdb()
+            show = tvdb[showname]
+            for season in show:
+                seasondata = tvdb[showname][season]
+                for ep in seasondata:
+                    epData = seasondata[ep]
+                    ep_date = datetime.datetime.strptime(epData['firstaired'],'%Y-%m-%d')
+                    check_date = datetime.datetime.utcfromtimestamp(int(epAirdate))
+                    if ep_date == check_date:
+                        logging.debug('MATCHED Season [' + str(season) + '] Episode [' + str(ep) + ']')
+                        season_str = str(season).zfill(2)
+                        return season_str
+            return
         if epNumber[0] == 'S':
             season_str = epNumber[1:3]
         else:
